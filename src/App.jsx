f48dccf419384ba707466c1f0bfe12ab460b4e37
Shogo Ogami <sg.tao.so@gmail.com>
2024-05-03 15:47:46 +0900

    Add:modify pages components to change shown component from uri

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SideMenu from './components/SideMenu.jsx';
 import HomePage from './pages/HomePage.jsx';
 import NESEmulator from './pages/NESEmulator.jsx';
@@ -6,14 +6,30 @@ import './App.css';
 
 function App() {
   // const [count, setCount] = useState(0);
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [currentQuery, setCurrentQuery] = useState(
+    new URLSearchParams(window.location.search).get('q'),
+  );
+
+  useEffect(() => {
+    const handlePathChange = () => {
+      setCurrentPath(window.location.pathname);
+      setCurrentQuery(new URLSearchParams(window.location.search).get('q'));
+    };
+
+    window.addEventListener('popstate', handlePathChange);
+    return () => {
+      window.removeEventListener('popstate', handlePathChange);
+    };
+  }, []);
 
   return (
     <>
       <div className="app-container">
         <SideMenu />
-        <main>
-          <HomePage />
-          <NESEmulator />
+        <main className="pages">
+          {currentPath !== '/Game' && <HomePage />}
+          {currentPath === '/Game' && <NESEmulator game={currentQuery} />}
         </main>
       </div>
     </>
--- src/pages/NESEmulator.jsx
+++ src/pages/NESEmulator.jsx
@@ -1,13 +1,23 @@
 // pages/NESEmulator.jsx
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './NESEmulator.css';
 
-const NESEmulator = () => {
+const NESEmulator = ({ game = '' }) => {
+  useEffect(() => {
+    console.log(`Game ${game} selected.`);
+  }, [game]);
+
   return (
     <div className="nes-emulator">
       <div className="emulator-screen">{/* NES emulator game screen */}</div>
       <div className="emulator-controls">{/* NES emulator controls */}</div>
     </div>
   );
+};
+
+NESEmulator.propTypes = {
+  game: PropTypes.string,
 };
 
 export default NESEmulator;
