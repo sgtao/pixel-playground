7526574a3cc3dab36925568a9bc478a89a736c92
Shogo Ogami <sg.tao.so@gmail.com>
2024-05-03 09:48:02 +0900

    Add:replace favicon to bun icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import viteLogo from '~/vite.svg';
+import bunLogo from '~/bun-logo.svg';
 import './App.css';
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <a href="https://bun.sh/" target="_blank">
+          <img src={bunLogo} className="logo" alt="bun logo" />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Bun + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
